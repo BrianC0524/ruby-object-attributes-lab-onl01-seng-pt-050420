ce2dc4469392a47a5323c13080e73cfc07d34bce
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-21 02:51:04 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+  
+  def name=(name)
+    
+    
+    
+  end
+  
+  def name
+    
+    
+    
+  end
+  
+  def breed=(breed)
+    
+    
+    
+  end
+  
+  def breed
+    
+    
+    
+  end
+  
+  
+  
+end
